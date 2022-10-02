ab0ba2d0d9afb3c8f3921824b698f9b23e9227c9
venkatesansubramanian <venkatesangit80@outlook.com>
2022-10-02 11:05:47 -0500

    Lambda Implementation

---
+++ range.py
@@ -0,0 +1,17 @@
+normal_range = range(6)
+start_to_end_range = range(3,9)
+odd_range = range(3,9,2)
+even_range = range(2,10,2)
+
+print('normal_range')
+for single_value in normal_range:
+    print(single_value)
+print('start_to_end_range')
+for single_value in start_to_end_range:
+    print(single_value)
+print('odd_range')
+for single_value in odd_range:
+    print(single_value)
+print('even_range')
+for single_value in even_range:
+    print(single_value)
