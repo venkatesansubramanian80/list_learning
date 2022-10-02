6b8a36fa21b8c15820fe2071d4f7a0d7091c508d
venkatesansubramanian <venkatesangit80@outlook.com>
2022-10-01 22:31:14 -0500

    List Implementation

---
+++ list.py
@@ -0,0 +1,9 @@
+lst_data = ['apple','orange','banana']
+print('Index Value : ' + lst_data[1])
+print('Negetive Index Value : ' + lst_data[-1])
+print(lst_data[:2])
+for single_value in lst_data:
+    print(single_value)
+lst_nested_data = ['apple','orange',['yellow_banana','red_banana']]
+print(lst_nested_data[1])
+print(lst_nested_data[2][1])
