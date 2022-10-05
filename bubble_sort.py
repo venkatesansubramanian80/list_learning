716f6897f1fbf03373164f4aca304309f336851a
venkatesansubramanian <venkatesangit80@outlook.com>
2022-10-05 18:55:03 -0500

    Bubble Sort Implementation

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def BubbleSort(list):
+    lastElementIndex = len(list) - 1
+    for passNo in range(lastElementIndex,0,-1):
+        for idx in range(passNo):
+            if list[idx] > list[idx+1]:
+                list[idx], list[idx+1] = list[idx+1], list[idx]
+    return list
+
+print(BubbleSort([25,21,22,24,23,27,26]))
