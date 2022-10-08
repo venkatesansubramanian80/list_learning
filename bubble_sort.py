29aa0509ca54d79726dfc951b87ab72248800036
venkatesansubramanian <venkatesangit80@outlook.com>
2022-10-08 15:31:39 -0500

    Bubble Sort Implementation - Line Explanations

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,7 @@
 def BubbleSort(list):
     lastElementIndex = len(list) - 1
     for passNo in range(lastElementIndex,0,-1):
+        print(range(passNo))
         for idx in range(passNo):
             if list[idx] > list[idx+1]:
                 list[idx], list[idx+1] = list[idx+1], list[idx]
