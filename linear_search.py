ffc88c4fb38e6f20d5f38bc84c7672ce36f93a45
venkatesansubramanian <venkatesangit80@outlook.com>
2022-10-09 16:32:23 -0500

    Linear Search Implementation

---
+++ linear_search.py
@@ -0,0 +1,13 @@
+def LinearSearch(list, item):
+    index = 0
+    found = False
+    while index < len(list) and found is False:
+        if list[index] == item:
+            found = True
+        else:
+            index = index + 1
+    return found
+
+list = [12,33,11,99,22,55,90]
+print(LinearSearch(list, 12))
+print(LinearSearch(list,91))
